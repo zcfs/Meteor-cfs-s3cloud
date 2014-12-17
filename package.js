f46a949ec4dbd824689a068935a5e0345674915d
Morten Henriksen <mh@gi2.dk>
2014-12-17 14:34:26 +0100

    bump versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cfs:s3cloud',
-  version: '0.0.0',
+  version: '0.0.5',
   summary: 'CollectionFS, S3 Cloud Storage Adapter',
   git: 'https://github.com/CollectionFS/Meteor-cfs-s3cloud.git'
 });
@@ -20,13 +20,13 @@ Package.onUse(function(api) {
     // Transfer is basically the up and download queues via ddp
     'cfs:transfer@0.0.14',
     // Want to make use of the file object and its api, yes!
-    'cfs:file@0.0.0',
+    'cfs:file@0.0.14',
     // Add the FS.Collection to keep track of everything
-    'cfs:collection@0.0.1',
+    'cfs:collection@0.5.0',
     // Add the option to have ddp and http access point
     'cfs:access-point@0.1.40',
     // We might also want to have the server create copies of our files?
-    'cfs:worker@0.0.0',
+    'cfs:worker@0.1.0',
   ]);
 
 
